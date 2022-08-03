ebdb97a646fea238359534b8b386ab6b2524c454
razzaksr <razzaksr@gmail.com>
2022-08-03 13:31:20 +0530

    list read done edit stared

--- Akash/basic/urls.py
+++ Akash/basic/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.hello),
+    # path('',views.hello),
     path('one',views.haiThere),
     path('two',views.show),
     path('log',views.logging),
-    path('new',views.callCreate)
+    path('new',views.callCreateOrEdit),
+    path('',views.callList),
+    path('<int:which>',views.callRead),
+    path('edit/<int:theone>',views.callCreateOrEdit)
     
 ]
 
--- Akash/basic/views.py
+++ Akash/basic/views.py
@@ -1,22 +1,54 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from . import models
 from . import forms
 
 # Create your views here.
 
-def callCreate(req):
+def callRead(req,which):
+    obj=models.Game.objects.get(id=which)
+    return render(req,"read.html",{"single":obj})
+
+def callList(req):
+    obj=models.Game.objects.all()
+    return render(req,"list.html",{"mylist":obj})
+
+
+def callCreateOrEdit(req,theone=0):
     if req.method=="POST":
-        object=forms.GameForm(req.POST)
+        if theone!=0:# update
+            ex=models.Game.objects.get(id=theone)
+            print(ex.name+" old")
+        else:
+            object=forms.GameForm(req.POST)
         if object.is_valid():
-            object.save()    
-            #return render("/giri/new")
-            obj=forms.GameForm()
-            return render(req,"create.html",{"hey":obj,"info":"New game added"})
+            object.save()
+            return redirect('/giri/');
         else:
             return render(req,"create.html",{"hey":object,"info":"New game not added"})
     else:
-        obj=forms.GameForm()
-        return render(req,"create.html",{"hey":obj,"info":"New game"})
+        if theone!=0:
+            obj=models.Game.objects.get(id=theone)
+            exist=forms.GameForm(instance=obj)
+            return render(req,"create.html",{"hey":exist,"title":"Update existing game"})
+        else:
+            obj=forms.GameForm()
+            return render(req,"create.html",{"hey":obj,"title":"New game creation"})
+    # for save alone
+    # if req.method=="POST":
+    #     object=forms.GameForm(req.POST)
+    #     if object.is_valid():
+    #         object.save()    
+    #         #return render("/giri/new")
+    #         obj=forms.GameForm()
+    #         #return render(req,"create.html",{"hey":obj,"info":"New game added"})
+    #         return redirect('/giri/');
+    #     else:
+    #         return render(req,"create.html",{"hey":object,"info":"New game not added"})
+    # else:
+    #     obj=forms.GameForm()
+    #     return render(req,"create.html",{"hey":obj,"info":"New game"})
 
 def logging(req):
     if req.method=="POST":
